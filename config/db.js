ac91937a51564293f9758de97140a1b24f906b5f
unknown <you@example.com>
2022-02-02 15:06:14 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+function connectDB() {
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true });
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Database connected');
+    })
+}
+module.exports = connectDB;
